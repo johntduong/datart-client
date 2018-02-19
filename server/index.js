b617ca27b8826730bc34952e363155218d7a3bad
johntduong <johntduong@yahoo.com>
2018-02-19 15:57:59 -0800

    testing routing

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 const port = process.env.PORT || 1234;
 app.listen(port, () => {
