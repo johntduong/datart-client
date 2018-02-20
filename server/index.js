b2eaa3f4966467e33098c3f2bbd7ce01e7218172
johntduong <johntduong@yahoo.com>
2018-02-19 16:10:39 -0800

    resolved path to serve file in server

--- server/index.js
+++ server/index.js
@@ -3,8 +3,9 @@ const app = express();
 const path = require("path");
 
 app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../index.html"));
 });
 
 const port = process.env.PORT || 1234;
