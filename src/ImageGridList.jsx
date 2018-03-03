cd019c38df621e22e2ea9448c355eca427faeb85
johntduong <johntduong@yahoo.com>
2018-03-03 00:37:20 -0800

    reverted to older markup up ImageGridList component

--- src/ImageGridList.jsx
+++ src/ImageGridList.jsx
@@ -5,41 +5,33 @@ import ImageGridItem from "./ImageGridItem";
 import images from "../data.json";
 
 class ImageGridList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isOpen: false,
-      imageSelected: "test.jpg"
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = e => {
-    console.log("EVENT.TARGET", e.target);
-    this.setState({
-      isOpen: !this.state.isOpen,
-      imageSelected: e.target.src
-    });
-  };
   render() {
     const { size: { width } } = this.props;
 
+    // {return (
+    //   <div className="grid-container">
+    //     <StackGrid
+    //       monitorImagesLoaded={true}
+    //       columnWidth={width <= 768 ? "100%" : "33.33%"}
+    //     >
+    //       {images.art.map(art => (
+    //         <ImageGridItem
+    //           key={art.itemNum}
+    //           value={art.poster}
+    //           handler={this.handleClick}
+    //           {...art}
+    //         />
+    //       ))}
+    //     </StackGrid>
+    //   </div>
+    // );}
     return (
-      <div className="grid-container">
-        <StackGrid
-          monitorImagesLoaded={true}
-          columnWidth={width <= 768 ? "100%" : "33.33%"}
-        >
-          {images.art.map(art => (
-            <ImageGridItem
-              key={art.itemNum}
-              value={art.poster}
-              handler={this.handleClick}
-              {...art}
-            />
-          ))}
-        </StackGrid>
-      </div>
+      <StackGrid
+        monitorImagesLoaded={true}
+        columnWidth={width <= 768 ? "100%" : "33.33%"}
+      >
+        {images.art.map(art => <ImageGridItem key={art.itemNum} {...art} />)}
+      </StackGrid>
     );
   }
 }
--- src/imageGridItem.jsx
+++ src/imageGridItem.jsx
@@ -1,20 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import images from "../data.json";
 
+// {const ImageGridItem = props => (
+//   <div
+//     className="grid-item-container"
+//     key={props.itemNum}
+//     onClick={props.handler}
+//   >
+//     <img
+//       className="gallery-img"
+//       alt={`Picture of sculpture titled ${props.title}`}
+//       src={`/images/gallery/${props.poster}`}
+//     />
+//     <div className="after">
+//       <p>Title: {props.title}</p>
+//       <p>Title: {props.title}</p>
+//     </div>
+//   </div>
+// );}
+
 const ImageGridItem = props => (
-  <div
-    className="grid-item-container"
-    key={props.itemNum}
-    onClick={props.handler}
-  >
+  <div className="grid-item-container" key="key1">
     <img
       className="gallery-img"
       alt={`Picture of sculpture titled ${props.title}`}
       src={`/images/gallery/${props.poster}`}
     />
-    <div className="after">
-      <p>Title: {props.title}</p>
-    </div>
+    <div className="after">This is some content</div>
   </div>
 );
 
