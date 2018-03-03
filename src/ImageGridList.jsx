6ce884da236228594a45585d26070f8ac48ff989
johntduong <johntduong@yahoo.com>
2018-03-03 00:08:05 -0800

    removed imageGridItem from list for testing

--- src/ImageGridList.jsx
+++ src/ImageGridList.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import sizeMe from "react-sizeme";
 import StackGrid from "react-stack-grid";
-import ImageGridItem from "./ImageGridItem.jsx";
 import images from "../data.json";
 
 class ImageGridList extends Component {
