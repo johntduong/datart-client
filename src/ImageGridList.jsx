325f4192d9d0f10d95ea77d82b6de3f355414b04
johntduong <johntduong@yahoo.com>
2018-03-03 00:18:34 -0800

    removed full picture feature from gallery

--- src/ImageGridList.jsx
+++ src/ImageGridList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import sizeMe from "react-sizeme";
 import StackGrid from "react-stack-grid";
+import ImageGridItem from "./ImageGridItem";
 import images from "../data.json";
 
 class ImageGridList extends Component {
@@ -23,19 +24,23 @@ class ImageGridList extends Component {
   render() {
     const { size: { width } } = this.props;
 
-    if (this.state.isOpen) {
-      return (
-        <div className="grid-container">
-          <div className="modal">
-            <div className="modal-slide-container">
-              <img className="modal-slide" src={this.state.imageSelected} />
-            </div>
-          </div>
-        </div>
-      );
-    } else {
-      return <div className="grid-container" />;
-    }
+    return (
+      <div className="grid-container">
+        <StackGrid
+          monitorImagesLoaded={true}
+          columnWidth={width <= 768 ? "100%" : "33.33%"}
+        >
+          {images.art.map(art => (
+            <ImageGridItem
+              key={art.itemNum}
+              value={art.poster}
+              handler={this.handleClick}
+              {...art}
+            />
+          ))}
+        </StackGrid>
+      </div>
+    );
   }
 }
 
